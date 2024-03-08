4db331c9cc9cd72bc2d00cc55e937fc5bb2f5df8
RazaMoosavi <rezamoosavi.kntu@gmail.com>
2024-03-08 21:42:24 +0330

    fix: add sass

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -1,22 +1,33 @@
-import type { StorybookConfig } from '@storybook/react-vite'
+import type { StorybookConfig } from '@storybook/react-vite';
 const config: StorybookConfig = {
   stories: ['../src/**/*.stories.@(js|jsx|ts|tsx)'],
   addons: [
     '@storybook/addon-links',
     '@storybook/addon-essentials',
     '@storybook/addon-interactions',
-    '@storybook/addon-styling',
+    {
+      name: '@storybook/addon-styling-webpack',
+      options: {
+        rules: [
+          // Replaces existing CSS rules with given rule
+          {
+            test: /\.css$/,
+            use: ['style-loader', 'css-loader'],
+          },
+        ],
+      },
+    },
   ],
   framework: {
     name: '@storybook/react-vite',
     options: {
       builder: {
-        viteConfigPath: '.storybook/vite.config.ts',
+        viteConfigPath: '.storybook/vite.config.mts',
       },
     },
   },
   docs: {
     autodocs: 'tag',
   },
-}
-export default config
+};
+export default config;
